55bb409332d54bfd03e9d494475a98b3b8fee02d
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-10-25 17:11:09 -0400

    added few common methods for data driven

--- src/test/java/hooks/appHooks.java
+++ src/test/java/hooks/appHooks.java
@@ -1,7 +1,5 @@
 package hooks;
 
-
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
@@ -23,73 +21,73 @@ import utilities.LoggerLoad;
 import utilities.PropertyLoader;
 
 public class appHooks {
-	
-	private static WebDriver driver;
-    private PropertyLoader propertyLoader;
 
-	
+	private static WebDriver driver;
+	private PropertyLoader propertyLoader;
+	private static String saveDir;
+
 	@Before
 	public void setup() throws Throwable {
-		 propertyLoader = new PropertyLoader();  // Initialize the PropertyLoader
-	        String browser = System.getProperty("browser"); // Get the browser from system properties
-
-	         if (browser == null || browser.isEmpty()) {
-	            browser = propertyLoader.getProperty("browser"); // Fetch browser property from config file
-	        }
-	         switch (browser.toLowerCase()) {
-	            case "chrome":
-	                driver = new ChromeDriver(new ChromeOptions());
-	                break;
-	            case "firefox":
-	                driver = new FirefoxDriver(new FirefoxOptions());
-	                break;
-	            default:
-	                throw new IllegalArgumentException("Unsupported browser: " + browser);
-	        }
-
-	        driver.manage().window().maximize();
+		propertyLoader = new PropertyLoader(); // Initialize the PropertyLoader
+		String browser = System.getProperty("browser"); // Get the browser from system properties
+		if (browser == null || browser.isEmpty()) {
+			browser = propertyLoader.getProperty("browser"); // Fetch browser property from config file
+		}
+		switch (browser.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver(new ChromeOptions());
+			break;
+		case "firefox":
+			driver = new FirefoxDriver(new FirefoxOptions());
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported browser: " + browser);
+		}
+		driver.manage().window().maximize();
 		CommonMethods.driver = driver;
-		   // Download the file before executing scenarios
-        downloadFile();
+		downloadFile(); // Download the file before executing scenarios
 	}
-	
+
 	@After
-	public void teardown(Scenario scenario) 
-	{
-if(driver!=null && scenario.isFailed()) {
-			
-			LoggerLoad.info("Taking screenshot for a failed sceanrio "+scenario);
-		
-			TakesScreenshot ts = (TakesScreenshot)driver;
-		
+	public void teardown(Scenario scenario) {
+		if (driver != null && scenario.isFailed()) {
+
+			LoggerLoad.info("Taking screenshot for a failed sceanrio " + scenario);
+
+			TakesScreenshot ts = (TakesScreenshot) driver;
+
 			byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-		
-			scenario.attach(screenshot, "image/png", "errorscreen");	
-		
+
+			scenario.attach(screenshot, "image/png", "errorscreen");
+
 			Allure.addAttachment("failed scenario screenshot", new ByteArrayInputStream(screenshot));
 
 		}
-		   if (driver != null) {
-			   LoggerLoad.info("Quitting the browser ");
-	            driver.quit();
-	            driver = null; 
-	        }
-	}
-	
-	 private void downloadFile() {
-	    	
-	        String fileURL = "https://docs.google.com/spreadsheets/d/1yJnBrcNqL196xoXOuVO99XOw4ACQLjAucmZEog3UdX4/export?format=xlsx";
-	        String saveDir = "src/test/resources/LMSData.xlsx"; 
 
-	        try {
-	            FileDownloader.downloadFile(fileURL, saveDir);
-	        } catch (IOException e) {
-	            e.printStackTrace(); // Handle error as necessary
-	        }
-	    }
-	 
-    public static WebDriver getDriver() {
-        return driver;
-    }
+		if (driver != null) {
+			LoggerLoad.info("Quitting the browser ");
+			driver.quit();
+			driver = null;
+		}
+	}
+
+	private void downloadFile() {
+
+		String fileURL = "https://docs.google.com/spreadsheets/d/1yJnBrcNqL196xoXOuVO99XOw4ACQLjAucmZEog3UdX4/export?format=xlsx";
+		saveDir = "src/test/resources/LMSData.xlsx";
+		try {
+			FileDownloader.downloadFile(fileURL, saveDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	public static String getSaveDir() {
+		return saveDir;
+	}
 
 }
--- src/test/java/stepDefinitions/Batch_BatchPageValidationSteps.java
+++ src/test/java/stepDefinitions/Batch_BatchPageValidationSteps.java
@@ -13,116 +13,93 @@ import pageObjects.LoginPage;
 import utilities.CommonMethods;
 import utilities.PropertyLoader;
 
-public class Batch_BatchPageValidationSteps extends CommonMethods{
+public class Batch_BatchPageValidationSteps extends CommonMethods {
 
-	
-	 WebDriver driver = appHooks.getDriver();
-	 LoginPage hp = new LoginPage(driver);
-	 DashboardPage dp = new DashboardPage(driver);
-	 LoginSteps ls = new LoginSteps();
-	 String excelPath = "src/test/resources/LMSData.xlsx"; // Specify your Excel file path
-	 BatchPage bp = new BatchPage(driver,excelPath);
-	   
-	 
-	 	private PropertyLoader propertyLoader;
-	    public Batch_BatchPageValidationSteps() {
-	        propertyLoader = new PropertyLoader();
-	    }
-	    
-	    
-	
+	WebDriver driver = appHooks.getDriver();
+	BatchPage bp = new BatchPage(driver);
+	DashboardPage dp = new DashboardPage(driver);
+	LoginSteps ls = new LoginSteps();
+
 	@Given("Admin is on the Dashboard page after Login")
 	public void admin_is_on_the_dashboard_page_after_login() {
 		getURL();
-    	ls.admin_enter_valid_credentials_and_clicks_login_button();;
-    	implicitlyWait();
-		
+		ls.admin_enter_valid_credentials_and_clicks_login_button();
+		;
+		implicitlyWait();
+
 	}
-	
-	 @When("Admin Clicks on the Batch menu from header")
-	    public void admin_clicks_on_the_batch_menu_from_header() {
-	    	
-	    	dp.clickBatchLink();
-	    	
-	    }
-	 
+
+	@When("Admin Clicks on the Batch menu from header")
+	public void admin_clicks_on_the_batch_menu_from_header() {
+
+		dp.clickBatchLink();
+
+	}
+
 	@Then("Admin should see the {string} Title")
 	public void admin_should_see_the_title(String string) {
-		  String title = getPageTitle(driver);
-		    Assert.assertEquals(title, "LMS");
+		String title = getPageTitle(driver);
+		Assert.assertEquals(title, "LMS");
 	}
-	
+
 	@Then("Admin should see the {string} Heading")
 	public void admin_should_see_the_heading(String string) {
 		String title = bp.getBatchTitle();
-   	 Assert.assertEquals(title, "Manage Batch");
+		Assert.assertEquals(title, "Manage Batch");
 	}
-	
-	
+
 	@Then("Admin should see the disabled {string} under the header")
 	public void admin_should_see_the_disabled_under_the_header(String string) {
-	Boolean actualValue = bp.clickDeleteIconManage();
+		Boolean actualValue = bp.clickDeleteIconManage();
 		Assert.assertEquals(false, actualValue);
-		
+
 	}
-	
-	
+
 	@Then("Admin should see the enabled pagination controls under the data table")
 	public void admin_should_see_the_enabled_pagination_controls_under_the_data_table() {
 		Boolean actualValue = bp.clickPaginationControl();
-		Assert.assertEquals(true, actualValue);	
+		Assert.assertEquals(true, actualValue);
 	}
 
-
-	
 	@Then("Admin should see the edit icon in each row")
 	public void admin_should_see_the_edit_icon_in_each_row() {
 		int ActualCount = bp.clickEditIcon();
 		Assert.assertEquals(ActualCount, 5);
 	}
-	
-	
+
 	@Then("Admin should see the delete icon in each row")
 	public void admin_should_see_the_delete_icon_in_each_row() {
 		int ActualCount = bp.clickDeleteIcon();
-		Assert.assertEquals(ActualCount, 5);	
+		Assert.assertEquals(ActualCount, 5);
 	}
-	
 
 	@Then("Admin should see the checkbox in each row")
 	public void admin_should_see_the_checkbox_in_each_row() {
-	  int ActualCount = bp.clickCheckBoxRow();
-		Assert.assertEquals(ActualCount, 5);		
+		int ActualCount = bp.clickCheckBoxRow();
+		Assert.assertEquals(ActualCount, 5);
 	}
-	
 
 	@Then("Admin should see the datatable headers Batch name, Batch Description,Batch Status, No Of classes, Program Name, Edit\\/Delete")
 	public void admin_should_see_the_datatable_headers_batch_name_batch_description_batch_status_no_of_classes_program_name_edit_delete() {
-		String[] actualHeaders = bp.verifyHeader();	
-		String[] expectedHeaders = {"Batch Name", "Batch Description", "Batch Status", "No Of Classes","Program Name" ,"Edit / Delete"}; 
+		String[] actualHeaders = bp.verifyHeader();
+		String[] expectedHeaders = { "Batch Name", "Batch Description", "Batch Status", "No Of Classes", "Program Name",
+				"Edit / Delete" };
 		Assert.assertEqualsNoOrder(actualHeaders, expectedHeaders);
 	}
-	
-	
+
 	@Then("Admin should see the checkbox  in the datatable header row")
 	public void admin_should_see_the_checkbox_in_the_datatable_header_row() {
-		 boolean actualValue = bp.clickCheckBoxHeader();
-		 System.out.println(actualValue);
-		 Assert.assertEquals(true, actualValue);	
-	}
-	
-	
-	
-	
-	@Then("Admin should see the sort icon next to all Datatable headers")
-	public void admin_should_see_the_sort_icon_next_to_all_datatable_headers() {
-	    
-		int ActualCount = bp.clickSortIconHeader();
-		Assert.assertEquals(ActualCount, 5);
-		
-		
+		boolean actualValue = bp.clickCheckBoxHeader();
+		System.out.println(actualValue);
+		Assert.assertEquals(true, actualValue);
 	}
 
-	
-	
+	@Then("Admin should see the sort icon next to all Datatable headers")
+	public void admin_should_see_the_sort_icon_next_to_all_datatable_headers() {
+
+		int ActualCount = bp.clickSortIconHeader();
+		Assert.assertEquals(ActualCount, 5);
+
+	}
+
 }
