eb10c5c450bb2ae701f8182ffa9e11ec183602d4
Shobana31 <dshobanamca@gmail.com>
2024-10-28 16:37:49 -0700

    added logoutsteps, dashboard steps, login steps git commit -m src/test/resources/features/TC05_LogoutPage.feature

--- src/test/java/pageObjects/DashboardPage.java
+++ src/test/java/pageObjects/DashboardPage.java
@@ -3,6 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashboardPage {
@@ -13,28 +14,39 @@ public WebDriver driver;
 		this.driver = driver;
 			PageFactory.initElements(driver, this);
 	}
-	
+
+	@FindBy(how = How.XPATH, using = "//body")
+	public WebElement DB_Body;
+
+	@FindBy(how = How.XPATH, using = "//span[normalize-space()='LMS - Learning Management System']")
+	public WebElement DB_Title;
+
+	@FindBy(how = How.XPATH, using = "//mat-toolbar[@class='mat-toolbar mat-primary mat-toolbar-single-row ng-star-inserted']//div[@class='ng-star-inserted']")
+	public WebElement DB_MenuItems;
+
 	//Locating Elements ////button[normalize-space()='Add New Program']
+
+	//mat-toolbar[@class='mat-toolbar mat-primary mat-toolbar-single-row ng-star-inserted']//div[@class='ng-star-inserted']/button[@id='program']
 	@FindBy(xpath="//span[normalize-space()='Program']")
-	WebElement lnkProgram;
+	public WebElement lnkProgram;
 	
 	@FindBy(xpath="//button[normalize-space()='Add New Program']")
-	WebElement lnkAddProgram;
+	public WebElement lnkAddProgram;
 	
 	@FindBy(xpath="//span[normalize-space()='Batch']")
-	WebElement lnkBatch;
+ 	public WebElement lnkBatch;
 
 	@FindBy(xpath="//button[normalize-space()='Add New Batch']")
-	WebElement lnkAddBatch;
+	public WebElement lnkAddBatch;
 	
 	@FindBy(xpath="//span[normalize-space()='Class']")
-	WebElement lnkClass;
+	public WebElement lnkClass;
 	
 	@FindBy(xpath="//button[normalize-space()='Add New Class']")
-	WebElement lnkAddClass;
+	public WebElement lnkAddClass;
 	
 	@FindBy(xpath="//span[normalize-space()='Logout']")
-	WebElement lnkLogout;
+	public WebElement lnkLogout;
 	
 	public void clickProgramLink() {
 		lnkProgram.click();
---
+++ src/test/java/stepDefinitions/LogoutSteps.java
@@ -0,0 +1,49 @@
+package stepDefinitions;
+
+import hooks.appHooks;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import pageObjects.DashboardPage;
+import pageObjects.LoginPage;
+import utilities.CommonMethods;
+import utilities.PropertyLoader;
+
+
+public class TC05_LogoutSteps extends CommonMethods {
+
+    WebDriver driver = appHooks.getDriver();
+    LoginPage lp = new LoginPage(driver);
+    DashboardPage dp = new DashboardPage(driver);
+    private PropertyLoader propertyLoader;
+
+    public TC05_LogoutSteps() {
+        propertyLoader = new PropertyLoader();
+    }
+
+    @Given("Admin landed in login Page")
+    public void admin_landed_in_login_page() {
+        implicitlyWait();
+        getURL();
+    }
+    @When("Admin enter valid credentials and clicks login button")
+    public void admin_enter_valid_credentials_and_clicks_login_button() {
+        lp.txtUserName.sendKeys(propertyLoader.getProperty("Username"));
+        lp.txtPassword.sendKeys(propertyLoader.getProperty("Password"));
+        lp.clickLogin();
+    }
+    @Given("Admin is in dashboard page")
+    public void admin_is_in_dashboard_page() {
+        Assert.assertEquals(getPageTitle(driver), "LMS");
+    }
+    @When("Admin clicks on the logout in the menu bar")
+    public void admin_clicks_on_the_logout_in_the_menu_bar() {
+       elementClick(dp.lnkLogout);
+    }
+    @Then("Admin should be redirected to login page")
+    public void admin_should_be_redirected_to_login_page() {
+        Assert.assertEquals(getPageTitle(driver), "LMS");
+    }
+}
